6852323237d2f6f1c71cd9dd14bda5c5369b0f2a
Marius Rackwitz <git@mariusrackwitz.de>
2014-09-01 17:06:07 +0200

    [Spec] Added a spec for Constants::TARGET_CONFIGURATIONS

--- spec/constants_spec.rb
+++ spec/constants_spec.rb
@@ -18,4 +18,23 @@ describe Xcodeproj::Constants do
       subject.should.all_conform? { |_,v| v.frozen? }
     end
   end
+
+  describe 'TARGET_CONFIGURATIONS' do
+    def subject
+      Xcodeproj::Constants::TARGET_CONFIGURATIONS
+    end
+
+    it 'has keys which matches their config_dir_name' do
+      subject.should.all_conform? { |k,v| v.config_dir_name == k }
+    end
+
+    it 'has values which are TargetConfigurations' do
+      subject.should.all_conform? { |_,v| v.is_a?(Xcodeproj::TargetConfiguration) }
+    end
+
+    it 'has for each value a dumped base config file for latest Xcode version' do
+      dir = Pathname(Dir[data_path('*/configs')].sort.last)
+      subject.map { |_,v| dir + v.base_config_file_path }.should.all_conform? &:exist?
+    end
+  end
 end
